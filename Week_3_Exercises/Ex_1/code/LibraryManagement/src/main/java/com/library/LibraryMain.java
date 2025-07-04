1f90aee6cec999f1582817a43ebb6241226bc9e3
Surendar K <surendarkumar2k@gmail.com>
2025-07-04 12:41:53 +0530

    completed exercise 2 springcoremaven

---
+++ Week_3_Exercises/Ex_1/code/LibraryManagement/src/main/java/com/library/LibraryMain.java
@@ -0,0 +1,18 @@
+package com.library;
+
+import com.library.service.BookService;
+import com.library.repository.BookRepository;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LibraryMain {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService bookService = (BookService) context.getBean("bookService");
+        BookRepository bookRepository = (BookRepository) context.getBean("bookRepository");
+
+        bookService.printServiceMessage();
+        bookRepository.printRepositoryMessage();
+    }
+}
---
+++ Week_3_Exercises/Ex_1/code/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,7 @@
+package com.library.service;
+
+public class BookService {
+    public void printServiceMessage() {
+        System.out.println("BookService is working.");
+    }
+}
